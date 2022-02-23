1814ed030f72f75bd4cb5dceacecef364c8e0785
scottmcn204 <77010334+scottmcn204@users.noreply.github.com>
2022-02-23 18:09:50 +0000

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 radio.setGroup(5)
 servos.P2.setAngle(0)
+// pins.servo_write_pin(AnalogPin.P2, 0)
 on_received_number(3)
 function on_received_number(receivedNumber: number) {
     if (receivedNumber == 0) {
@@ -12,8 +13,9 @@ function on_received_number(receivedNumber: number) {
         `)
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
-        servos.P2.setAngle(0)
+        servos.P2.setAngle(90)
     } else if (receivedNumber == 1) {
+        // pins.servo_write_pin(AnalogPin.P2, 0)
         basic.showLeds(`
         # # # # #
         # . . . .
@@ -25,6 +27,7 @@ function on_received_number(receivedNumber: number) {
         pins.digitalWritePin(DigitalPin.P1, 1)
         servos.P2.setAngle(45)
     } else if (receivedNumber == 3) {
+        // pins.servo_write_pin(AnalogPin.P2, 30)
         basic.showLeds(`
         . . . . .
         . # # # .
@@ -34,11 +37,12 @@ function on_received_number(receivedNumber: number) {
         `)
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 0)
-        servos.P2.setAngle(0)
+        servos.P2.setAngle(90)
     }
     
 }
 
+// pins.servo_write_pin(AnalogPin.P2, 0)
 radio.onReceivedNumber(on_received_number)
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     on_received_number(0)
